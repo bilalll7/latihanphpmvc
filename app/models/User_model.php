86e756ccaf76705a13fa0cda9351a45ed973e528
bilalll7 <albilalbintang909@gmail.com>
2023-11-24 09:42:34 +0700

    first commit

---
+++ app/models/User_model.php
@@ -0,0 +1,8 @@
+<?php
+class User_model{
+  private $nama = 'Bilal';
+  public function getUser(){
+    return $this->nama;
+  }
+}
+?>
---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+  <h1 class="mt-4">My About</h1>
+  <img src="<?= BASEURL; ?>/img/logoo.png" alt="" width="200" class="rounded-circle shadow mb-5">
+  <p>Halo, Nama Saya adalah <?= $data['nama']?>,Saya adalah Seorang <?= $data['pekerjaan']?> dan Saya Berumur <?= $data['umur']?></p>
+  </div>
